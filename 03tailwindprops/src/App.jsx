23e4a39ba471643c3e4786ca133ca104f68fcfdd
Ariba Husain <aribahusain19082004@gmail.com>
2024-09-19 20:28:39 +0530

    props in React

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Card from './Components/Card'
+function App() {
+  
+
+  return (
+    <>
+      <h1 >Ariba</h1>
+      <Card last="end of card"/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
